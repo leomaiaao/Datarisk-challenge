037720d71bc8a854387c1e274fe464480a436de1
Leonardo Maia Assis Oliveira <leomaiaao@gmail.com>
2023-04-18 13:06:45 -0300

    A lot of changes to make it work

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,10 +10,10 @@ terraform {
 }
 
 provider "azurerm" {
-  subscription_id = "${env.ARM_SUBSCRIPTION_ID}"
-  client_id       = "${env.ARM_CLIENT_ID}"
-  client_secret   = "${env.ARM_CLIENT_SECRET}"
-  tenant_id       = "${env.ARM_TENANT_ID}"
+  subscription_id = "${var.ARM_SUBSCRIPTION_ID}"
+  client_id       = "${var.ARM_CLIENT_ID}"
+  client_secret   = "${var.ARM_CLIENT_SECRET}"
+  tenant_id       = "${var.ARM_TENANT_ID}"
 }
 
 resource "azurerm_resource_group" "DatariskVM" {
